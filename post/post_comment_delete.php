f04bfb59b05a5ab6350b7ca196b0aaee3e0a6a4a
huynhkhoinguyen251 <emailken098@gmail.com>
2025-05-05 02:34:34 +0700

    update

--- post/post_comment_delete.php
+++ post/post_comment_delete.php
@@ -53,29 +53,6 @@ if (isset($_GET['confirm']) && $_GET['confirm'] === 'yes') {
     }
 }
 
+require '../templates/post_comment_delete.html.php';
+require '../footer.php';
 ?>
-
-<div class="container">
-    <h1>Delete Comment</h1>
-    
-    <?php if ($error): ?>
-        <div class="alert error">
-            <?= htmlspecialchars($error) ?>
-        </div>
-    <?php endif; ?>
-    
-    <div class="confirmation-box">
-        <p>Are you sure you want to delete this comment?</p>
-        <div class="comment">
-            <div class="comment-content">
-                <p><?= htmlspecialchars($comment['content']) ?></p>
-            </div>
-        </div>
-        <div class="actions">
-            <a href="post_comment_delete.php?id=<?= $comment_id ?>&confirm=yes" class="btn btn-delete"><i class="fas fa-trash-alt"></i> Yes, Delete</a>
-            <a href="../post/post.php?id=<?= $post_id ?>" class="btn"><i class="fas fa-times"></i> Cancel</a>
-        </div>
-    </div>
-</div>
-
-<?php require '../footer.php'; ?> 
--- templates/post_comment.html.php
+++ templates/post_comment.html.php
@@ -1,5 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -42,4 +40,3 @@
         </form>
     </div>
 </body>
-</html>
